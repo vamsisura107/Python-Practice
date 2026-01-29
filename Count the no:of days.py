2691c3e03599feb1737624243539604989a7f8a8
SURA VAMSI KRISHNA REDDY <suravamsikrishnareddy@gmail.com>
2026-01-29 14:09:38 +0530

    Count the no:of days.py

---
+++ Count the no:of days.py
@@ -0,0 +1,14 @@
+month = 12
+year=2012
+    
+if((month==2) and ((year%4==0)  or ((year%100==0) and (year%400==0)))) :
+    print("Number of days is 29");
+
+elif(month==2) :
+    print("Number of days is 28");
+
+elif(month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12) :
+    print("Number of days is 31");
+
+else :
+    print("Number of days is 30");
