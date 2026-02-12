4ca77712100ad905a27b9f3cea94ce267ee4384b
SURA VAMSI KRISHNA REDDY <suravamsikrishnareddy@gmail.com>
2026-02-12 10:38:31 +0530

    Create Frequency of elements in an array.py

---
+++ Frequency of elements in an array.py
@@ -0,0 +1,12 @@
+arr = [10, 30, 10, 20, 10, 30, 20, 10]
+
+visited = []
+
+for i in range(len(arr)):
+    if arr[i] not in visited:
+        count = 0
+        for j in range(len(arr)):
+            if arr[i] == arr[j]:
+                count += 1
+        print(arr[i], count)
+        visited.append(arr[i])
